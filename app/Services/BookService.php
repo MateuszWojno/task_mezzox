c8a19f51e3eddbcafa3fbcd5f77131c339945716
MateuszWojno <mateusz.wojno9922@gmail.com>
2024-03-07 00:58:32 +0100

    Add getList(), getPaginatedData() and transformData() in BookService

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -11,6 +11,44 @@ class BookService
     {
     }
 
+    public function getList()
+    {
+        $customersList = $this->getPaginatedData();
+
+        $transformedData = $this->transformData($customersList->getCollection());
+
+        return [
+            'data' => $transformedData,
+            'meta' => [
+                'total' => $customersList->total(),
+                'per_page' => $customersList->perPage(),
+                'current_page' => $customersList->currentPage(),
+                'last_page' => $customersList->lastPage(),
+            ]
+        ];
+    }
+
+    protected function getPaginatedData()
+    {
+        $perPage = 20;
+        return $this->model::paginate($perPage);
+    }
+
+    protected function transformData($data)
+    {
+        return $data->map(function($customer) {
+            return [
+                'author' => $customer->author,
+                'status' => $customer->status,
+                'borrower' => [
+                    'first_name' => $customer->borrower->first_name,
+                    'last_name' => $customer->borrower->last_name,
+                ]
+            ];
+        });
+    }
+
+
     public function getItem($id)
     {
         $customer = $this->model->findOrFail($id);
