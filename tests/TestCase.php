b58743a4e278ed58729d108923ed5c66e3f75f94
MateuszWojno <mateusz.wojno9922@gmail.com>
2024-03-06 21:46:00 +0100

    Add method to create admin and user for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,32 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Hash;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function createUsers(): void
+    {
+        $this->admin = User::create([
+            'first_name'        => fake()->firstName(),
+            'last_name'         => fake()->lastName(),
+            'email'             => fake()->email(),
+            'email_verified_at' => Carbon::now(),
+            'password'          => Hash::make('asd')
+        ]);
+        $this->admin->addRole('admin');
+        $this->user = User::create([
+            'first_name'        => fake()->firstName(),
+            'last_name'         => fake()->lastName(),
+            'email'             => fake()->email(),
+            'email_verified_at' => Carbon::now(),
+            'password'          => Hash::make('asd')
+        ])->addRole('user');
+
+    }
 }
