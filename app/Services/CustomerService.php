ab71687e7c1bda89e02bab3b98cf6cd5dce1b881
MateuszWojno <mateusz.wojno9922@gmail.com>
2024-03-08 02:09:19 +0100

    Add getCustomerDetails() in CustomerService

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Book;
 use App\Models\Customer as CustomerModel;
 use App\Transformers\CustomerTransformer;
 
@@ -19,6 +20,62 @@ class CustomerService
             ->collection($customersList, new CustomerTransformer())
             ->toArray();
     }
+
+    public function getCustomerDetails($id)
+    {
+        $customer = $this->model->findOrFail($id);
+        $borrowedBooks = Book::where('borrower_id', $customer->id)->get();
+
+        $responseData = [
+            'first_name'     => $customer->first_name,
+            'last_name'      => $customer->last_name,
+            'borrowed_books' => [],
+        ];
+
+
+        if ($borrowedBooks->isNotEmpty()) {
+            $responseData['borrowed_books'] = $borrowedBooks->map(function ($book) {
+                return [
+                    'title'            => $book->title,
+                    'author'           => $book->author,
+                    'publication_year' => $book->publication_year,
+                    'publisher'        => $book->publisher,
+                    'status'           => $book->status,
+                ];
+            });
+
+        }
+
+        return $responseData;
+    }
+
+
+//    public function getCustomerDetails($id)
+//    {
+//        $customer = $this->model->findOrFail($id);
+//        $borrowedBooks = Book::where('borrower_id', $customer->id)->get();
+//
+//        // Inicjalizacja Fractal Managera
+//        $fractal = new Manager();
+//
+//        // Wykorzystanie transformera dla klienta
+//        $customerResource = new Item($customer, new CustomerTransformer());
+//
+//        // Wykorzystanie transformera dla wypożyczonych książek
+//        $booksResource = new Collection($borrowedBooks, new BookTransformer());
+//
+//        // Pobranie przekształconych danych
+//        $responseData = [
+//            'customer' => $fractal->createData($customerResource)->toArray(),
+//            'borrowed_books' => $fractal->createData($booksResource)->toArray(),
+//        ];
+//
+//        $abc = $responseData;
+//        dd($abc);
+//        return $abc;
+//
+//    }
+
 
     public function create(array $attributes)
     {
@@ -41,6 +98,5 @@ class CustomerService
             ->item($deleted, new CustomerTransformer())
             ->toArray();
     }
-
 
 }
